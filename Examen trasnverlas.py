bf69ffb24f2400b4a64e4e31320c5dd0510c06c5
dempox9 <cla.correa@duocuc.cl>
2025-07-08 21:11:08 -0400

    Add files via upload

---
+++ Examen trasnverlas.py
@@ -0,0 +1,4 @@
+print("\tBienvenidos a mi programa!")
+print("\t===========================")
+
+print("\nEvaluacion Examen transveral de Programacion y Redes Virtualizadas\nCon:\nPaulina Cerda\nClaudio Correa\nOctavio Alou")
